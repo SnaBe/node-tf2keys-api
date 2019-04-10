6da9b75513419f642c96fe42c6c6cf069b3cf440
Simon Sørensen <fynboen1999@gmail.com>
2019-04-10 21:48:37 +0200

    Syntax

--- examples/basicrequest.js
+++ examples/basicrequest.js
@@ -14,18 +14,18 @@ var API = new tf2api({
 API.all((err, res, data) => {
   if(err) {
     //Some error occurred during your request
-  	console.log(err)
+    console.log(err)
   } else {
   	//Do something with the price data
-  	console.log(data);
+    console.log(data);
   	//Save the data
   	API.save(data, (err, status) => {
   	  if(err) {
         //Some error saving the data
-  	  	console.log(err);
+        console.log(err);
   	  } else {
         //Log the display
-  	  	console.log(status);
+        console.log(status);
   	  }
   	});
   }
