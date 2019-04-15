f2350358a55d7fbd44c5a41f6da555dc8ea07a57
Simon Sørensen <fynboen1999@gmail.com>
2019-04-15 18:41:33 +0200

    Url update
    New API request urls

--- resources/urls.js
+++ resources/urls.js
@@ -1,6 +1,6 @@
 module.exports = {
   all: 'http://localhost:3000/all',
   search: 'http://localhost:3000/search',
-  add: '',
-  remove: ''
+  add: 'http://localhost:3000/add',
+  remove: 'http://localhost:3000/remove'
 }
