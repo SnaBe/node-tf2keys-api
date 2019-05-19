5919faedac3d0a75887cb45ae7c2043d62f8cfb6
Simon Sørensen <fynboen1999@gmail.com>
2019-05-19 16:27:11 +0200

    New url for future update

--- resources/urls.js
+++ resources/urls.js
@@ -2,5 +2,6 @@ module.exports = {
   all: 'http://localhost:3000/all',
   search: 'http://localhost:3000/search',
   add: 'http://localhost:3000/add',
-  remove: 'http://localhost:3000/remove'
+  remove: 'http://localhost:3000/remove',
+  request: 'http://localhost:3000/request'
 }
