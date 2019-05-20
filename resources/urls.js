4ec7c1819ad34ce2c09b7b7015fc87924102466d
Simon Sørensen <fynboen1999@gmail.com>
2019-05-20 16:02:50 +0200

    New server ulrs for testing

--- resources/urls.js
+++ resources/urls.js
@@ -1,7 +1,8 @@
 module.exports = {
-  all: 'http://localhost:3000/all',
-  search: 'http://localhost:3000/search',
-  add: 'http://localhost:3000/add',
-  remove: 'http://localhost:3000/remove',
-  request: 'http://localhost:3000/request'
+  home: 'http://139.59.145.128:3000/',
+  all: 'http://139.59.145.128:3000/all',
+  search: 'http://139.59.145.128:3000/search',
+  add: 'http://139.59.145.128:3000/add',
+  remove: 'http://139.59.145.128:3000/remove',
+  request: 'http://139.59.145.128:3000/request'
 }
