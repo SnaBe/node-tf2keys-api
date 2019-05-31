8ed0cd786d69070890ba9e60e011b250f15bd137
Simon Sørensen <fynboen1999@gmail.com>
2019-05-31 16:07:24 +0200

    Update urls.js

--- resources/urls.js
+++ resources/urls.js
@@ -1,8 +1,8 @@
 module.exports = {
-  home: 'http://139.59.145.128:3000/',
-  all: 'http://139.59.145.128:3000/all',
-  search: 'http://139.59.145.128:3000/search',
-  add: 'http://139.59.145.128:3000/add',
-  remove: 'http://139.59.145.128:3000/remove',
-  request: 'http://139.59.145.128:3000/request'
+  home: '/',
+  all: '/all',
+  search: '/search',
+  add: '/add',
+  remove: '/remove',
+  request: '/request'
 }
